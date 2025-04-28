b240e76e4a815199e5bd8857ca366c105845bef2
renil <renilgarala360004@gmail.com>
2025-04-28 22:19:57 +0530

    Connect with Postgres using Prisma

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,18 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
 
 dotenv.config();
+
 const app = express();
+
+app.use(express.json());
+
 const PORT = process.env.PORT||3000;
 
 app.get("/", (req, res)=>{
     res.send("Hello");
 })
+
+
 
 app.use(cookieParser);
 
