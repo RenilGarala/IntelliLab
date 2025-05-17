42ce056c6514d3954b11aa6a852213a3d9ddc54b
renil <renilgarala360004@gmail.com>
2025-05-17 16:40:42 +0530

    feat: add getSubmissionForProblem controller to fetch submissions for a specific problem

--- backend/src/controllers/submission.controllers.js
+++ backend/src/controllers/submission.controllers.js
@@ -28,6 +28,41 @@ export const getAllSubmission = async (req, res) => {
   }
 };
 
-export const getSubmissionForProblem = async (req, res) => {};
+export const getSubmissionForProblem = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    if (!userId) {
+      return res.status(400).json({
+        message: "user not found",
+      });
+    }
+
+    const problemId = req.params.problemId;
+
+    if (!problemId) {
+      return res.status(400).json({
+        message: "problemId not found",
+      });
+    }
+
+    const submissions = await db.submission.findMany({
+        where:{
+            userId:userId,
+            problemId:problemId,
+        }
+    })
+
+    return res.status(200).json({
+        success: true,
+        message: "Submission Fatched Successfully",
+        submissions
+    })
+  } catch (error) {
+    return res.status(500).json({
+        error: "Failed to fatch submissions",
+      });
+  }
+};
 
 export const getAllSubmissionForProblem = async (req, res) => {};
