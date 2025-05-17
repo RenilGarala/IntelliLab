cb4ab389453379d53bb588eb4495f29aa5ecfbb7
renil <renilgarala360004@gmail.com>
2025-05-17 14:42:42 +0530

    feat: add runCode controller in execute.controller.js to handle code execution

--- backend/src/controllers/execute.controllers.js
+++ backend/src/controllers/execute.controllers.js
@@ -151,3 +151,69 @@ export const executeCode = async (req, res) => {
     });
   }
 };
+
+export const runCode = async (req, res) => {
+  const { source_code, language_id, stdin, expected_outputs, problemId } =
+    req.body;
+
+  try {
+    if (
+        !Array.isArray(stdin) ||
+        stdin.length === 0 ||
+        !Array.isArray(expected_outputs) ||
+        expected_outputs.length !== stdin.length
+      ) {
+        return res.status(400).json({
+          message: "Invalid or Missing test cases",
+        });
+      }
+  
+      //prepare each test case for judge0 batch submission
+      const submissions = stdin.map((input) => ({
+        source_code,
+        language_id,
+        stdin: input,
+      }));
+  
+      //sent this submission to judge0
+      const submitResponse = await submitBatch(submissions);
+  
+      const tokens = submitResponse.map((req) => req.token);
+  
+      //pull judge0 for result of all submitted test cases
+      const result = await pollBatchResults(tokens);
+  
+      //check testcase results with status id 3
+      let allPassed = true;
+      const detailedResults = result.map((result, i) => {
+        const stdout = result.stdout?.trim();
+        const expected_output = expected_outputs[i]?.trim();
+  
+        const passed = stdout === expected_output;
+        if (!passed) {
+          allPassed = false;
+        }
+  
+        return {
+          testCase: i + 1,
+          passed,
+          stdout,
+          expected: expected_output,
+          stderr: result.stderr || null,
+          compile_output: result.compile_output || null,
+          status: result.status.description,
+          memory: result.memory ? `${result.memory} KB` : undefined,
+          time: result.time ? `${result.time} s` : undefined,
+        };
+      });
+
+      return res.status(200).json({
+        message: allPassed ? "Accepted" : "Wrong Answer",
+        results: detailedResults,
+      });
+  } catch (error) {
+    return res.status(500).json({
+        message: "Problem can't run"
+    });
+  }
+};
--- backend/src/routes/execute.routes.js
+++ backend/src/routes/execute.routes.js
@@ -1,9 +1,11 @@
 import express from "express"
 import { authenticate } from "../middleware/auth.middleware.js";
-import { executeCode } from "../controllers/execute.controllers.js";
+import { executeCode, runCode } from "../controllers/execute.controllers.js";
 
 const executionRoute = express.Router();
 
-executionRoute.post("/", authenticate, executeCode)
+executionRoute.post("/", authenticate, executeCode);
+
+executionRoute.post("/run-code", authenticate, runCode);
 
 export default executionRoute;
