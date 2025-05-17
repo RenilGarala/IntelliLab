03559f947799a783d27a18c2f380a3f3ef9d4325
renil <renilgarala360004@gmail.com>
2025-05-17 16:49:30 +0530

    feat: add getSubmissionsCount controller to return total number of submissions

--- backend/src/controllers/submission.controllers.js
+++ backend/src/controllers/submission.controllers.js
@@ -47,22 +47,56 @@ export const getSubmissionForProblem = async (req, res) => {
     }
 
     const submissions = await db.submission.findMany({
-        where:{
-            userId:userId,
-            problemId:problemId,
-        }
-    })
+      where: {
+        userId: userId,
+        problemId: problemId,
+      },
+    });
 
     return res.status(200).json({
-        success: true,
-        message: "Submission Fatched Successfully",
-        submissions
-    })
+      success: true,
+      message: "Submission Fatched Successfully",
+      submissions,
+    });
   } catch (error) {
     return res.status(500).json({
-        error: "Failed to fatch submissions",
-      });
+      error: "Failed to fatch submissions",
+    });
   }
 };
 
-export const getAllSubmissionForProblem = async (req, res) => {};
+export const getAllSubmissionForProblem = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    if (!userId) {
+      return res.status(400).json({
+        message: "user not found",
+      });
+    }
+
+    const problemId = req.params.problemId;
+
+    if (!problemId) {
+      return res.status(400).json({
+        message: "problem id not found",
+      });
+    }
+
+    const submissionCount = await db.submission.count({
+      where: {
+        problemId: problemId,
+      },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Submission count fatched",
+      submissionCount,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: "Failed to fatch submissions",
+    });
+  }
+};
