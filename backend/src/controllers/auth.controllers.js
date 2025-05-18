30ab29789f795d130d4f2628a4fc1dd59f3edeea
renil <renilgarala360004@gmail.com>
2025-05-18 12:57:56 +0530

    feat: add getSubmissions controller in auth.controller.js

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,173 +1,187 @@
 import bcrypt from "bcryptjs";
 import { db } from "../libs/db.js";
 import { UserRole } from "../generated/prisma/index.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-export const register = async (req,res)=>{
-    const {email, password, name} = req.body;
+export const register = async (req, res) => {
+  const { email, password, name } = req.body;
 
-    if(!name || !email || !password){
-        return res.status(400).json({
-            message: "All field is required",
-        });
+  if (!name || !email || !password) {
+    return res.status(400).json({
+      message: "All field is required",
+    });
+  }
+
+  if (password.length < 6) {
+    return res.status(400).json({
+      message: "Password must be in 6 letter",
+    });
+  }
+
+  try {
+    const existingUser = await db.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
     }
 
-    if(password.length < 6){
-        return res.status(400).json({
-            message: "Password must be in 6 letter",
-        });
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const newUser = await db.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+        role: UserRole.USER,
+      },
+    });
+
+    const token = jwt.sign({ id: newUser.id }, process.env.JWT_KEY, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        id: newUser.id,
+        email: newUser.email,
+        name: newUser.name,
+        role: newUser.role,
+        image: newUser.image,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: "An error occurred while creating the user.",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(401).json({
+      message: "All fields are required.",
+    });
+  }
+
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      req.status(400).json({
+        message: "User does not exist. Please check your email and try again.",
+      });
     }
 
-    try {
-        const existingUser = await db.user.findUnique({
-            where:{
-                email:email
-            }
-        })
-  
-        if(existingUser){
-            return res.status(400).json({
-                message: "User already exists",
-            });
-        }
+    const isMatch = await bcrypt.compare(password, user.password);
 
-        const hashedPassword = await bcrypt.hash(password, 12);
-
-        const newUser = await db.user.create({
-            data:{
-                email,
-                password: hashedPassword,
-                name,
-                role: UserRole.USER
-            }
-        })
-
-        const token = jwt.sign(
-            { id: newUser.id },           
-            process.env.JWT_KEY,           
-            { expiresIn: "7d" }          
-          );
-
-        res.cookie("jwt", token , {
-            httpOnly: true,
-            sameSite: "strict",
-            secure: process.env.NODE_ENV !== "development",
-            maxAge: 1000 * 60 * 60 * 24 * 7
-        });
-
-        res.status(201).json({
-            success: true,
-            message: "User created successfully",
-            user:{
-                id: newUser.id,
-                email: newUser.email,
-                name: newUser.name,
-                role: newUser.role,
-                image: newUser.image,
-            }
-        })
-    } catch (error) {
-        return res.status(500).json({
-            error: "An error occurred while creating the user.",
-        });
+    if (!isMatch) {
+      res.status(401).json({
+        message: "Invalid email or password. Please try again.",
+      });
     }
 
-}
+    const token = jwt.sign({ id: user.id }, process.env.JWT_KEY, {
+      expiresIn: "7d",
+    });
 
-export const login = async (req,res)=>{
-    const {email, password} = req.body;
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
 
-    if(!email || !password){
-        res.status(401).json({
-            message: "All fields are required."
-        })
-    }
+    res.status(201).json({
+      success: true,
+      message: "User logged in successfully.",
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role,
+        image: user.image,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: "An error occurred while logging in.",
+    });
+  }
+};
 
-    try {
-        const user = await db.user.findUnique({
-            where:{
-                email:email
-            }
-        })
-    
-        if(!user){
-            req.status(400).json({
-                message: "User does not exist. Please check your email and try again."
-            })
-        }
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+    });
 
-        const isMatch = await bcrypt.compare(password, user.password);
+    res.status(200).json({
+      success: true,
+      message: "User logged out successfully.",
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Something went wrong during logout. Please try again.",
+    });
+  }
+};
 
-        if(!isMatch){
-            res.status(401).json({
-                message: "Invalid email or password. Please try again."
-            })
-        }
+export const checkAuth = async (req, res) => {
+  try {
+    //extract user from middleware and send as a response
+    res.status(200).json({
+      success: true,
+      message: "User Authenticate Successfully",
+      user: req.user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Unauthorized access. Login required.",
+    });
+  }
+};
 
-        const token = jwt.sign({id: user.id}, process.env.JWT_KEY, {expiresIn: "7d"});
+export const getSubmissions = async (req, res) => {
+  try {
+    const submissions = await db.submission.findMany({
+      where: {
+        userId: req.user.id,
+      },
+    });
 
-        res.cookie("jwt", token , {
-            httpOnly: true,
-            sameSite: "strict",
-            secure: process.env.NODE_ENV !== "development",
-            maxAge: 1000 * 60 * 60 * 24 * 7
-        });
+    return res.status(200).json({
+      success: true,
+      message: "Submissions fetched successfully",
+      submissions,
+    });
+  } catch (error) {
+    return res.status(500).json({ 
+        error: "Failed to fetch submissions" 
+    });
+  }
+};
 
-        res.status(201).json({
-            success: true,
-            message: "User logged in successfully.",
-            user:{
-                id: user.id,
-                email: user.email,
-                name: user.name,
-                role: user.role,
-                image: user.image,
-            }
-        })
-
-    } catch (error) {
-        return res.status(500).json({
-            error: "An error occurred while logging in.",
-        });
-    }
-}
-
-export const logout = async (req,res)=>{
-    try {
-        res.clearCookie("jwt",{
-            httpOnly: true,
-            sameSite: "strict",
-            secure: process.env.NODE_ENV !== "development",
-        })
-
-        res.status(200).json({
-            success: true,
-            message: "User logged out successfully."
-        })
-    } catch (error) {
-        res.status(400).json({
-            message: "Something went wrong during logout. Please try again."
-        });      
-    }
-}
-
-export const checkAuth = async (req,res)=>{
-    try {
-        //extract user from middleware and send as a response
-        res.status(200).json({
-            success: true,
-            message: "User Authenticate Successfully",
-            user: req.user,
-        })
-    } catch (error) {
-        res.status(400).json({
-            message: "Unauthorized access. Login required."
-        })
-    }
-}
-
-export const getSubmission = async (req, res)=>{
-}
-
-export const getUserPlaylists = async (req, res)=>{
-}
+export const getUserPlaylists = async (req, res) => {};
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { checkAuth, login, logout, register } from "../controllers/auth.controllers.js";
+import { checkAuth, getSubmissions, login, logout, register } from "../controllers/auth.controllers.js";
 import { authenticate } from "../middleware/auth.middleware.js";
 
 const authRoutes = express.Router();
@@ -11,5 +11,7 @@ authRoutes.post("/login", login);
 authRoutes.post("/logout", authenticate, logout);
 
 authRoutes.get("/check", authenticate, checkAuth);
+
+authRoutes.get("/get-submissions",authenticate , getSubmissions);
 
 export default authRoutes;
