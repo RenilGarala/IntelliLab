59f06043dc282aa36f2ae2b4a964e0b4d64efb7e
renil <renilgarala360004@gmail.com>
2025-05-18 14:34:25 +0530

    feat: define playlist-related routes and controller functions

---
+++ backend/src/routes/playlist.routes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { authenticate } from "../middleware/auth.middleware.js";
+import { addProblemToPlaylist, createPlaylist, deletePlaylist, getAllListDetail, getPlaylistDetails, removeProblemFromPlaylist } from "../controllers/playlist.controllers.js";
+
+const playlistRoutes = express.Router();
+
+playlistRoutes.get('/', authenticate, getAllListDetail);
+
+playlistRoutes.get('/:playlistId', authenticate, getPlaylistDetails);
+
+playlistRoutes.post('/create-playlist', authenticate, createPlaylist);
+
+playlistRoutes.post('/:plalistId/add-problem', authenticate, addProblemToPlaylist);
+
+playlistRoutes.delete('/:plalistId', authenticate, deletePlaylist);
+
+playlistRoutes.delete('/:plalistId/remove-problem', authenticate, removeProblemFromPlaylist);
+
+export default playlistRoutes;
