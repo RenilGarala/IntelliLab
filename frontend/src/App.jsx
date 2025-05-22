5344764d1796ab36a04b07cb2337ced229efec8b
renil <renilgarala360004@gmail.com>
2025-05-22 23:51:37 +0530

    feat: design and implement sign-up page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
 
 const App = () => {
+  let authUser = null;
   return (
-    <div>
-      IntelliLab
+    <div className='flex flex-col items-center justify-start'>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={!authUser ? <LoginPage/> : <Navigate to={"/"}/>}/>
+        <Route path='/signup' element={!authUser ? <SignUpPage/>: <Navigate to={"/"}/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,172 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Link } from "react-router-dom";
+import { Eye, EyeOff } from "lucide-react";
+import { z } from "zod";
+import myImage from "../assets/Logo3.png";
+import { useAuthStore } from "../store/useAuthStore";
+
+const SignUpSchema = z.object({
+  email: z.string().email("Enter a Valid Email").trim(),
+
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters long")
+    .max(100, "Password must not exceed 100 characters")
+    .regex(
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]/,
+      "Password must include at least one letter and one number"
+    )
+    .trim(),
+
+  name: z
+    .string()
+    .min(2, "Name must be at least 2 characters")
+    .max(50, "Name must not exceed 50 characters")
+    .regex(/^[a-zA-Z\s]+$/, "Name can only contain letters and spaces")
+    .trim(),
+});
+
+const SignUpPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const {signup , isSigninUp} = useAuthStore();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(SignUpSchema),
+  });
+
+  const onSubmit = async (data) => {
+    try {
+      await signup(data);
+      console.log("SignUp Data:", data);
+    } catch (error) {
+      console.error("SignUp failed:", error);
+    }
+  };
+
+  return (
+    <div className="h-screen grid">
+      <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+        <div className="w-full max-w-md space-y-8">
+          <div className="text-center mb-8">
+            <div className="flex flex-col items-center gap-2 group">
+              <div className="w-12 h-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
+                <img src={myImage} alt="logo" />
+              </div>
+              <h1 className="text-2xl font-bold mt-2">Welcome</h1>
+              <p className="text-base-content/60">Sign Up to your account</p>
+            </div>
+          </div>
+
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Name</span>
+              </label>
+              <div className="relative">
+                <input
+                  type="text"
+                  {...register("name")}
+                  className={`input input-bordered w-full ${
+                    errors.name ? "input-error" : ""
+                  }`}
+                  placeholder="John Doe"
+                />
+              </div>
+              {errors.name && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.name.message}
+                </p>
+              )}
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Email</span>
+              </label>
+              <div className="relative">
+                <input
+                  type="email"
+                  {...register("email")}
+                  className={`input input-bordered w-full ${
+                    errors.email ? "input-error" : ""
+                  }`}
+                  placeholder="you@example.com"
+                />
+              </div>
+              {errors.email && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Password</span>
+              </label>
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  {...register("password")}
+                  className={`input input-bordered w-full ${
+                    errors.password ? "input-error" : ""
+                  }`}
+                  placeholder="••••••••"
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <EyeOff className="h-5 w-5 text-base-content/40" />
+                  ) : (
+                    <Eye className="h-5 w-5 text-base-content/40" />
+                  )}
+                </button>
+              </div>
+              {errors.password && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+
+            <button
+              type="submit"
+              className="btn btn-primary w-full"
+              //   disabled={isSigninUp}
+            >
+              {/* {isSigninUp ? (
+                <>
+                  <Loader2 className="h-5 w-5 animate-spin" />
+                  Loading...
+                </>
+              ) : (
+                "Sign in"
+              )} */}
+              Sign in
+            </button>
+          </form>
+
+          <div className="text-center">
+            <p className="text-base-content/60">
+              Already have an account?{" "}
+              <Link to="/login" className="link link-primary">
+                Sign in
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
