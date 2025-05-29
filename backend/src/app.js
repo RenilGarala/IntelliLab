b13a86127f4b5be2b40005d5c59be4a4219a73cf
renil <renilgarala360004@gmail.com>
2025-05-30 02:27:34 +0530

    fix: trying to solve deploy errors, correction in cors configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,6 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "http://localhost:4173",
       "https://intelli-lab.vercel.app",
       "https://www.intellilab.in",
     ],
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-import.meta.env.MODE = "production"
-console.log(import.meta.env.MODE);
-
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
