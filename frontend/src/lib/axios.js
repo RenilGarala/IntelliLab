ccc858faf62db6672befff548a4df582401a6eaa
renil <renilgarala360004@gmail.com>
2025-05-30 00:39:22 +0530

    issue

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import.meta.env.MODE = "production"
 console.log(import.meta.env.MODE);
 
+
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
