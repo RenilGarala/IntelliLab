214898810f638d6029a7655f723473d9c4d80f64
renil <renilgarala360004@gmail.com>
2025-05-30 00:52:25 +0530

    issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "http://localhost:5173",
+      "https://intelli-lab.vercel.app",
+      "https://www.intellilab.in",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
