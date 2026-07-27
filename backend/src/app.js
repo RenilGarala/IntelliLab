67d331e46cb6232e7e2c809d352cdc593be5e185
Renil Garala <132253013+RenilGarala@users.noreply.github.com>
2026-07-27 22:09:58 +0530

    Add new CORS origin for intellilab.renil.in

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,6 +20,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://www.intellilab.in",
+      "https://intellilab.renil.in",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
